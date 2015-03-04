13d8d817069871c8931aed9b4183fa35ff082b9b
nickbarth <nickbarth@live.com>
2015-03-04 04:10:58 -0700

    added title screen

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public Transform target;
-	public float timer;
+  public Transform target;
+  public float timer;
 
-	void Update () {
-		if (!Player.Dead) {
-			transform.position = new Vector3 (target.position.x, transform.position.y, transform.position.z);
-			timer = 0f;
-		} else {
-			timer += Time.deltaTime;
-			transform.position = Vector3.Lerp(transform.position, new Vector3(Player.LastCheckPoint.transform.position.x, transform.position.y, transform.position.z), timer / 2);
-		}
-	}
+  void Update () {
+    if (!Player.Dead) {
+      transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+      timer = 0f;
+    } else {
+      timer += Time.deltaTime;
+      transform.position = Vector3.Lerp(transform.position, new Vector3(Player.LastCheckPoint.transform.position.x, transform.position.y, transform.position.z), timer / 2);
+    }
+  }
 }
---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour {
+  public float timer;
+
+  // Config
+  public Vector3 StartPosition;
+  public Vector3 EndPosition;
+  public Vector3 TargetPosition;
+
+  public GameObject Title;
+  public Vector3 TitleStartPosition;
+  public Vector3 TitleEndPosition;
+  public Vector3 TitleTargetPosition;
+
+  public GameObject Text;
+
+  void Awake() {
+    GameFader.StartScene();
+    TargetPosition = EndPosition;
+    TitleTargetPosition = TitleEndPosition;
+  }
+
+  void Start() {
+    #if UNITY_IOS || UNITY_ANDROID
+    Text.GetComponent<Text>().text = "Press Screen to Start";
+    #endif
+  }
+
+  void Update() {
+    timer += Time.deltaTime;
+    transform.position = Vector3.Lerp(transform.position, TargetPosition, timer / 10);
+    Title.transform.position = Vector3.Lerp(Title.transform.position, TitleTargetPosition, timer / 10);
+
+    if (Input.GetMouseButtonDown(0) || Input.GetKey(KeyCode.Space)) {
+      GameFader.EndScene("Level1");
+    }
+
+    if (transform.position == EndPosition) {
+      timer = 0;
+      TargetPosition = StartPosition;
+      TitleTargetPosition = TitleStartPosition;
+    } else if (transform.position == StartPosition) {
+      timer = 0;
+      TargetPosition = EndPosition;
+      TitleTargetPosition = TitleEndPosition;
+    }
+  }
+}
